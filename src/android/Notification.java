85075004236402c0d8d4e64b57ae77d7e06eb2d9
cjmartin6162 <cmartin6162@gmail.com>
2021-10-02 20:29:23 -0700

    wert

--- src/android/Notification.java
+++ src/android/Notification.java
@@ -361,8 +361,8 @@ public class Notification extends CordovaPlugin {
         Runnable runnable = new Runnable() {
             public void run() {
                 Builder dlg = createDialog(cordova); // new AlertDialog.Builder(cordova.getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
-                dlg.setMessage("Monkies Butt");
-                dlg.setTitle("King");
+                dlg.setMessage(message);
+                dlg.setTitle(title);
                 dlg.setCancelable(true);
 
                 // First button
